468b6cf5a446b8a6edd918cced5fcda9f37c7e4c
Seevii16 <sevillanito435@gmail.com>
2022-03-05 17:59:24 +0100

    Añadir colores

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
             colors: {
                 "azul-claro": "#0CB7F2",
                 "azul-oscuro": "#0370b9",
-                "amarillo": "#FFC803"
+                "amarillo": "#FFC803",
+                "arena": "#E3D5AC"
             },
             animation: {
                 'spin-slow': 'spin 3s linear infinite'
